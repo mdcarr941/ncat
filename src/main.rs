28f8b2ef587f0bc41b34e0494aae59ef12ae0402
Matthew Carr <mdcarr941@gmail.com>
2021-07-26 18:38:56 -0400

    Implemented the client and server functions.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use std::env;
+use std::io;
+use std::io::Result;
+use std::net::{TcpListener, TcpStream};
 
 const DEFAULT_PORT: u16 = 41270;
 
-fn main() {
+fn main() -> Result<()> {
     let args: Vec<String> = env::args().skip(1).collect();
     match parse_command_line(args) {
-        Command::Client(host, port) => println!("Client: '{}':'{}'", host, port),
-        Command::Server(host, port) => println!("Server: '{}':'{}'", host, port)
+        Command::Client(host, port) => run_client(host, port),
+        Command::Server(host, port) => run_server(host, port)
     }
 }
 
@@ -36,14 +39,48 @@ fn parse_host_and_port(arg: String) -> (String, u16) {
     }
 }
 
+fn run_server(host: String, port: u16) -> Result<()> {
+    let listener = TcpListener::bind((host, port))?;
+
+    for result in listener.incoming() {
+        match result {
+            Ok(stream) => {
+                handle_client(stream)?;
+                break;
+            }
+            Err(error) => {
+                println!("{:?}", error);
+                continue;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn handle_client(mut stream: TcpStream) -> Result<()> {
+    let mut stdout = io::stdout();
+    io::copy(&mut stream, &mut stdout)?;
+    Ok(())
+}
+
+fn run_client(host: String, port: u16) -> Result<()> {
+    let mut stream = TcpStream::connect((host, port))?;
+    let mut stdin = io::stdin();
+    io::copy(&mut stdin, &mut stream)?;
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn test_parse_command_line_server() {
         let args = vec!["-l".to_string(), "localhost:1337".to_string()];
 
-        match super::parse_command_line(args) {
-            super::Command::Server(host, port) => {
+        match parse_command_line(args) {
+            Command::Server(host, port) => {
                 assert_eq!("localhost", host);
                 assert_eq!(1337, port);
             },
@@ -53,11 +90,11 @@ mod tests {
 
     #[test]
     fn test_parse_command_line_client() {
-        let args = vec!["carrfound.org:4221".to_string()];
+        let args = vec!["example.com:4221".to_string()];
 
-        match super::parse_command_line(args) {
-            super::Command::Client(host, port) => {
-                assert_eq!("carrfound.org", host);
+        match parse_command_line(args) {
+            Command::Client(host, port) => {
+                assert_eq!("example.com", host);
                 assert_eq!(4221, port);
             },
             _ => panic!("parse_command_line has a bug!")
@@ -68,10 +105,10 @@ mod tests {
     fn test_parse_command_line_server_default_port() {
         let args = vec!["-l".to_string(), "localhost".to_string()];
 
-        match super::parse_command_line(args) {
-            super::Command::Server(host, port) => {
+        match parse_command_line(args) {
+            Command::Server(host, port) => {
                 assert_eq!("localhost", host);
-                assert_eq!(super::DEFAULT_PORT, port);
+                assert_eq!(DEFAULT_PORT, port);
             },
             _ => panic!("parse_command_line has a bug!")
         }
