3ba14bfedbbc464410d5879dcef2631231f36324
Matthew Carr <mdcarr941@gmail.com>
2021-08-01 11:15:21 -0400

    Standardized error handling using Result.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 /* Copyright (c) 2021 Matthew Carr. Licensed under the GNU GPL version 3. */
 
 use std::env;
+use std::fmt::Display;
 use std::io;
-use std::io::Result;
 use std::net::{TcpListener, TcpStream};
 
 const DEFAULT_PORT: u16 = 41270;
 
+type Result<T> = std::result::Result<T, String>;
+
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().skip(1).collect();
-    match parse_command_line(args) {
+    match Command::new(args)? {
         Command::Client(host, port) => run_client(host, port),
         Command::Server(host, port) => run_server(host, port)
     }
@@ -21,28 +23,34 @@ enum Command {
     Server(String, u16)
 }
 
-fn parse_command_line(mut args: Vec<String>) -> Command {
-    if 0 == args.len() {
-        panic!("Too few command line arguments provided.");
+impl Command {
+    fn new(mut args: Vec<String>) -> Result<Command> {
+        if 0 == args.len() {
+            return Err("Too few command line arguments provided".to_string());
+        }
+        let last = args.pop().ok_or("Could not get the last command line argument")?;
+        let (host, port) = Command::parse_host_and_port(last)?;
+        match args.pop().as_deref() {
+            Some("-l") => Ok(Command::Server(host, port)),
+            _ => Ok(Command::Client(host, port))
+        }
     }
-    let last = args.pop().unwrap();
-    let (host, port) = parse_host_and_port(last);
-    match args.pop().as_deref() {
-        Some("-l") => Command::Server(host, port),
-        _ => Command::Client(host, port)
-    }
-}
-
-fn parse_host_and_port(arg: String) -> (String, u16) {
-    match arg.find(':') {
-        Some(index) =>
-            (arg[..index].to_string(), arg[(index + 1)..].parse::<u16>().unwrap()),
-        None => (arg, DEFAULT_PORT)
+    
+    fn parse_host_and_port(arg: String) -> Result<(String, u16)> {
+        match arg.find(':') {
+            Some(index) => {
+                let port = arg[(index + 1)..].parse::<u16>()
+                    .or_else(format_error)?;
+                Ok((arg[..index].to_string(), port))
+            },
+            None => Ok((arg, DEFAULT_PORT))
+        }
     }
 }
 
 fn run_server(host: String, port: u16) -> Result<()> {
-    let listener = TcpListener::bind((host, port))?;
+    let listener = TcpListener::bind((host, port))
+        .or_else(format_error)?;
 
     for result in listener.incoming() {
         match result {
@@ -51,7 +59,7 @@ fn run_server(host: String, port: u16) -> Result<()> {
                 break;
             }
             Err(error) => {
-                println!("{:?}", error);
+                eprintln!("{:?}", error);
                 continue;
             }
         }
@@ -62,15 +70,20 @@ fn run_server(host: String, port: u16) -> Result<()> {
 
 fn handle_client(mut stream: TcpStream) -> Result<()> {
     let mut stdout = io::stdout();
-    io::copy(&mut stream, &mut stdout)?;
+    io::copy(&mut stream, &mut stdout).or_else(format_error)?;
     Ok(())
 }
 
 fn run_client(host: String, port: u16) -> Result<()> {
-    let mut stream = TcpStream::connect((host, port))?;
+    let mut stream = TcpStream::connect((host, port)).or_else(format_error)?;
     let mut stdin = io::stdin();
-    io::copy(&mut stdin, &mut stream)?;
+    io::copy(&mut stdin, &mut stream).or_else(format_error)?;
     Ok(())
+}
+
+/// Formats the given error as a String.
+fn format_error<T, E: Display>(err: E) -> Result<T> {
+    Err(format!("{}", err))
 }
 
 #[cfg(test)]
@@ -81,7 +94,7 @@ mod tests {
     fn test_parse_command_line_server() {
         let args = vec!["-l".to_string(), "localhost:1337".to_string()];
 
-        match parse_command_line(args) {
+        match Command::new(args).unwrap() {
             Command::Server(host, port) => {
                 assert_eq!("localhost", host);
                 assert_eq!(1337, port);
@@ -94,7 +107,7 @@ mod tests {
     fn test_parse_command_line_client() {
         let args = vec!["example.com:4221".to_string()];
 
-        match parse_command_line(args) {
+        match Command::new(args).unwrap() {
             Command::Client(host, port) => {
                 assert_eq!("example.com", host);
                 assert_eq!(4221, port);
@@ -107,7 +120,7 @@ mod tests {
     fn test_parse_command_line_server_default_port() {
         let args = vec!["-l".to_string(), "localhost".to_string()];
 
-        match parse_command_line(args) {
+        match Command::new(args).unwrap() {
             Command::Server(host, port) => {
                 assert_eq!("localhost", host);
                 assert_eq!(DEFAULT_PORT, port);
